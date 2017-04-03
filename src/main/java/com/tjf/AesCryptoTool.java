1eeeecb07c06d9fd824caa8c3cecfb4969aca83c
frommeyer <t.frommeyer@gmail.com>
2017-04-03 17:03:11 -0400

    uncommented Base64 code

--- src/main/java/com/tjf/AesCryptoTool.java
+++ src/main/java/com/tjf/AesCryptoTool.java
@@ -4,7 +4,7 @@ import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.util.Arrays;
-//import java.util.Base64;
+import java.util.Base64;
  
 
 
